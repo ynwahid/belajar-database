70de543ffc0421b3b20e3a2e97af25840bff9124
ynw99 <yusufedu99@gmail.com>
2021-10-25 13:20:31 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Catatan Belajar Database
---
+++ belajar-mysql-dasar/barang.sql
@@ -0,0 +1,10 @@
+SHOW ENGINES;
+
+SHOW TABLES;
+
+CREATE TABLE barang(
+	id INT,
+    nama VARCHAR(100),
+    harga INT,
+    jumlah INT
+)ENGINE = InnoDB;
