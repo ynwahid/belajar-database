67203aa964ab6dc005827cc0832783d7135530c0
ynw99 <yusufedu99@gmail.com>
2021-10-25 13:38:30 +0700

    alter table

--- belajar-mysql-dasar/barang.sql
+++ belajar-mysql-dasar/barang.sql
@@ -8,3 +8,24 @@ CREATE TABLE barang(
     harga INT,
     jumlah INT
 )ENGINE = InnoDB;
+
+DESCRIBE barang;
+
+DESC barang;
+
+SHOW CREATE TABLE barang;
+
+ALTER TABLE barang
+ADD COLUMN deskripsi TEXT;
+
+ALTER TABLE barang
+ADD COLUMN salah TEXT;
+
+ALTER TABLE barang
+DROP COLUMN salah;
+
+ALTER TABLE barang
+MODIFY nama VARCHAR(200) AFTER deskripsi;
+
+ALTER TABLE barang
+MODIFY nama VARCHAR(200) FIRST;
