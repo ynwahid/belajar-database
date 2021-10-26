f5d910ada56225b1a03d40b1bcaa056a76312004
ynw99 <yusufedu99@gmail.com>
2021-10-26 13:40:06 +0700

    numerical function

--- belajar-mysql-dasar/products.sql
+++ belajar-mysql-dasar/products.sql
@@ -1,12 +1,11 @@
-CREATE TABLE products
-(
-	id VARCHAR(10) NOT NULL,
+CREATE TABLE products (
+    id VARCHAR(10) NOT NULL,
     name VARCHAR(100) NOT NULL,
     description TEXT,
     price INT UNSIGNED NOT NULL,
     quantity INT UNSIGNED NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE = InnoDB;
+)  ENGINE=INNODB;
 
 SHOW TABLES;
 
@@ -15,7 +14,10 @@ DESCRIBE products;
 INSERT INTO products(id, name, price, quantity)
 VALUES ('P0001', 'Mie Ayam Original', 15000, 100);
 
-SELECT * FROM products;
+SELECT 
+    *
+FROM
+    products;
 
 INSERT INTO products(id, name, description, price, quantity)
 VALUES ('P0002', 'Mie Ayam Bakso', 'Mie Ayam Original + Bakso', 20000, 100);
@@ -30,11 +32,20 @@ VALUES 	('P0006', 'Tahu Bulat', 5000, 150),
 		('P0007', 'Bakso Original', 10000, 200),
 		('P0008', 'Nasi Goreng', 15000, 150);
 
-SELECT * FROM products;
+SELECT 
+    *
+FROM
+    products;
         
-SELECT id, name, price, quantity FROM products;
+SELECT 
+    id, name, price, quantity
+FROM
+    products;
 
-SELECT name, quantity, id FROM products;
+SELECT 
+    name, quantity, id
+FROM
+    products;
 
 ALTER TABLE products
 ADD PRIMARY KEY (id);
@@ -46,11 +57,26 @@ SHOW CREATE TABLE products;
 INSERT INTO products(id, name, price, quantity)
 VALUES ('P0001', 'Mie Ayam Original', 15000, 100);
 
-SELECT * FROM products WHERE quantity = 100;
+SELECT 
+    *
+FROM
+    products
+WHERE
+    quantity = 100;
 
-SELECT * FROM products WHERE price = 15000;
+SELECT 
+    *
+FROM
+    products
+WHERE
+    price = 15000;
 
-SELECT * FROM products WHERE name = 'mie ayam bakso';
+SELECT 
+    *
+FROM
+    products
+WHERE
+    name = 'mie ayam bakso';
 
 DESCRIBE products;
 
@@ -58,51 +84,71 @@ ALTER TABLE products
 ADD COLUMN category ENUM('Makanan', 'Minuman', 'Lain-lain')
 AFTER name;
 
-SELECT * FROM products;
+SELECT 
+    *
+FROM
+    products;
 
-SELECT * FROM products WHERE id = 'P0001';
+SELECT 
+    *
+FROM
+    products
+WHERE
+    id = 'P0001';
 
-UPDATE products
-SET category = 'Makanan'
-WHERE id = 'P0001';
+UPDATE products 
+SET 
+    category = 'Makanan'
+WHERE
+    id = 'P0001';
 
-UPDATE products
-SET category = 'Makanan'
-WHERE id IN ('P0002', 'P0004', 'P0005');
+UPDATE products 
+SET 
+    category = 'Makanan'
+WHERE
+    id IN ('P0002' , 'P0004', 'P0005');
 
-UPDATE products
-SET category = 'Makanan',
-	description = 'Mie Ayam Original + Ceker'
-WHERE id = 'P0003';
+UPDATE products 
+SET 
+    category = 'Makanan',
+    description = 'Mie Ayam Original + Ceker'
+WHERE
+    id = 'P0003';
 
-UPDATE products
-SET price = price + 5000
-WHERE id = 'P0005';
+UPDATE products 
+SET 
+    price = price + 5000
+WHERE
+    id = 'P0005';
 
 INSERT INTO products(id, name, price, quantity)
 VALUES ('P0009', 'Mie Ayam Original', 15000, 100);
 
-DELETE
-FROM products
-WHERE id = 'P0009';
+DELETE FROM products 
+WHERE
+    id = 'P0009';
 
-DELETE
-FROM products
-WHERE id IN ('P0006', 'P0007', 'P0008');
+DELETE FROM products 
+WHERE
+    id IN ('P0006' , 'P0007', 'P0008');
 
-SELECT id AS Kode,
-	   name AS Nama,
-       category AS Kategori,
-       price AS Harga,
-       quantity AS Jumlah
-FROM products;
+SELECT 
+    id AS Kode,
+    name AS Nama,
+    category AS Kategori,
+    price AS Harga,
+    quantity AS Jumlah
+FROM
+    products;
 
-SELECT p.id AS Kode,
-	   p.name AS Nama,
-       p.category AS Kategori,
-       p.price AS Harga,
-       p.quantity AS Jumlah
-FROM products as p;
+SELECT 
+    p.id AS Kode,
+    p.name AS Nama,
+    p.category AS Kategori,
+    p.price AS Harga,
+    p.quantity AS Jumlah
+FROM
+    products AS p;
 
 INSERT INTO products(id, category, name, price, quantity)
 VALUES ('P0006', 'Makanan', 'Bakso Rusuk', 25000, 200),
@@ -116,56 +162,206 @@ VALUES ('P0006', 'Makanan', 'Bakso Rusuk', 25000, 200),
        ('P0014', 'Makanan', 'Bakso Telor', 20000, 150),
        ('P0015', 'Makanan', 'Bakso Jando', 25000, 300);
 	
-SELECT * FROM products WHERE quantity > 100;
+SELECT 
+    *
+FROM
+    products
+WHERE
+    quantity > 100;
 
-SELECT * FROM products WHERE quantity >= 100;
+SELECT 
+    *
+FROM
+    products
+WHERE
+    quantity >= 100;
 
-SELECT * FROM products WHERE category != 'Makanan';
+SELECT 
+    *
+FROM
+    products
+WHERE
+    category != 'Makanan';
 
-SELECT * FROM products WHERE category <> 'Minuman';
+SELECT 
+    *
+FROM
+    products
+WHERE
+    category <> 'Minuman';
 
-SELECT * FROM products WHERE quantity > 100 AND price > 20000;
+SELECT 
+    *
+FROM
+    products
+WHERE
+    quantity > 100 AND price > 20000;
 
-SELECT * FROM products WHERE category = 'Makanan' AND price < 20000;
+SELECT 
+    *
+FROM
+    products
+WHERE
+    category = 'Makanan' AND price < 20000;
 
-SELECT * FROM products WHERE quantity > 100 OR price > 20000;
+SELECT 
+    *
+FROM
+    products
+WHERE
+    quantity > 100 OR price > 20000;
 
-SELECT * FROM products WHERE (category = 'Makanan' OR quantity > 500) AND price > 20000;
+SELECT 
+    *
+FROM
+    products
+WHERE
+    (category = 'Makanan' OR quantity > 500)
+        AND price > 20000;
 
-SELECT * FROM products WHERE name LIKE '%mie%';
+SELECT 
+    *
+FROM
+    products
+WHERE
+    name LIKE '%mie%';
 
-SELECT * FROM products WHERE name LIKE '%bakso%';
+SELECT 
+    *
+FROM
+    products
+WHERE
+    name LIKE '%bakso%';
 
-SELECT * FROM products WHERE name LIKE '%usu%';
+SELECT 
+    *
+FROM
+    products
+WHERE
+    name LIKE '%usu%';
 
-SELECT * FROM products WHERE description IS NULL;
+SELECT 
+    *
+FROM
+    products
+WHERE
+    description IS NULL;
 
-SELECT * FROM products WHERE description IS NOT NULL;
+SELECT 
+    *
+FROM
+    products
+WHERE
+    description IS NOT NULL;
 
-SELECT * FROM products WHERE price BETWEEN 10000 AND 20000;
+SELECT 
+    *
+FROM
+    products
+WHERE
+    price BETWEEN 10000 AND 20000;
 
-SELECT * FROM products WHERE price NOT BETWEEN 10000 AND 20000;
+SELECT 
+    *
+FROM
+    products
+WHERE
+    price NOT BETWEEN 10000 AND 20000;
 
-SELECT * FROM products WHERE category IN ('Makanan', 'Minuman');
+SELECT 
+    *
+FROM
+    products
+WHERE
+    category IN ('Makanan' , 'Minuman');
 
-SELECT * FROM products WHERE category NOT IN ('Makanan', 'Minuman');
+SELECT 
+    *
+FROM
+    products
+WHERE
+    category NOT IN ('Makanan' , 'Minuman');
 
-SELECT id, category, name FROM products ORDER BY category;
+SELECT 
+    id, category, name
+FROM
+    products
+ORDER BY category;
 
-SELECT id, category, price, name FROM products ORDER BY category ASC, price DESC;
+SELECT 
+    id, category, price, name
+FROM
+    products
+ORDER BY category ASC , price DESC;
 
-SELECT * FROM products ORDER BY id LIMIT 5;
+SELECT 
+    *
+FROM
+    products
+ORDER BY id
+LIMIT 5;
 
-SELECT * FROM products ORDER BY id LIMIT 3;
+SELECT 
+    *
+FROM
+    products
+ORDER BY id
+LIMIT 3;
 
-SELECT * FROM products ORDER BY id LIMIT 0, 5;
+SELECT 
+    *
+FROM
+    products
+ORDER BY id
+LIMIT 0 , 5;
 
-SELECT * FROM products ORDER BY id LIMIT 5, 5;
+SELECT 
+    *
+FROM
+    products
+ORDER BY id
+LIMIT 5 , 5;
 
-SELECT * FROM products ORDER BY id LIMIT 10, 5;
+SELECT 
+    *
+FROM
+    products
+ORDER BY id
+LIMIT 10 , 5;
 
-SELECT * FROM products ORDER BY id LIMIT 15, 5;
+SELECT 
+    *
+FROM
+    products
+ORDER BY id
+LIMIT 15 , 5;
 
-SELECT category FROM products;
+SELECT 
+    category
+FROM
+    products;
 
-SELECT DISTINCT category FROM products;
+SELECT DISTINCT
+    category
+FROM
+    products;
+    
+SELECT 10, 10, 10 * 10 AS Hasil;
+
+SELECT 
+    id, name, price, price DIV 1000 AS 'Price in K'
+FROM
+    products;
+
+SELECT pi();
+
+SELECT power(10, 2);
+
+SELECT 
+    id, COS(price), SIN(price), TAN(price)
+FROM
+    products;
+    
+SELECT id, name, price
+FROM products
+WHERE price DIV 1000 > 15;
