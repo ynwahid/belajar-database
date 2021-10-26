7a9cd75dc4b0c4bcca1b537ed771905b21ba3cf2
ynw99 <yusufedu99@gmail.com>
2021-10-26 10:35:47 +0700

    Alias

--- belajar-mysql-dasar/products.sql
+++ belajar-mysql-dasar/products.sql
@@ -81,3 +81,17 @@ VALUES ('P0009', 'Mie Ayam Original', 15000, 100);
 DELETE
 FROM products
 WHERE id = 'P0009';
+
+SELECT id AS Kode,
+	   name AS Nama,
+       category AS Kategori,
+       price AS Harga,
+       quantity AS Jumlah
+FROM products;
+
+SELECT p.id AS Kode,
+	   p.name AS Nama,
+       p.category AS Kategori,
+       p.price AS Harga,
+       p.quantity AS Jumlah
+FROM products as p;
