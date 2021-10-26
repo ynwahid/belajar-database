74b715fbce0aa5b6c25aec5c21dfb0c4c931b0ab
ynw99 <yusufedu99@gmail.com>
2021-10-26 11:07:35 +0700

    Where operator

--- belajar-mysql-dasar/products.sql
+++ belajar-mysql-dasar/products.sql
@@ -82,6 +82,10 @@ DELETE
 FROM products
 WHERE id = 'P0009';
 
+DELETE
+FROM products
+WHERE id IN ('P0006', 'P0007', 'P0008');
+
 SELECT id AS Kode,
 	   name AS Nama,
        category AS Kategori,
@@ -95,3 +99,49 @@ SELECT p.id AS Kode,
        p.price AS Harga,
        p.quantity AS Jumlah
 FROM products as p;
+
+INSERT INTO products(id, category, name, price, quantity)
+VALUES ('P0006', 'Makanan', 'Bakso Rusuk', 25000, 200),
+       ('P0007', 'Minuman', 'Es Jeruk', 10000, 300),
+       ('P0008', 'Minuman', 'Es Campur', 15000, 500),
+       ('P0009', 'Minuman', 'Es Teh Manis', 5000, 400),
+       ('P0010', 'Lain-Lain', 'Kerupuk', 2500, 1000),
+       ('P0011', 'Lain-Lain', 'Keripik Udang', 10000, 300),
+       ('P0012', 'Lain-Lain', 'Es Krim', 5000, 200),
+       ('P0013', 'Makanan', 'Mie Ayam Jamur', 20000, 50),
+       ('P0014', 'Makanan', 'Bakso Telor', 20000, 150),
+       ('P0015', 'Makanan', 'Bakso Jando', 25000, 300);
+	
+SELECT * FROM products WHERE quantity > 100;
+
+SELECT * FROM products WHERE quantity >= 100;
+
+SELECT * FROM products WHERE category != 'Makanan';
+
+SELECT * FROM products WHERE category <> 'Minuman';
+
+SELECT * FROM products WHERE quantity > 100 AND price > 20000;
+
+SELECT * FROM products WHERE category = 'Makanan' AND price < 20000;
+
+SELECT * FROM products WHERE quantity > 100 OR price > 20000;
+
+SELECT * FROM products WHERE (category = 'Makanan' OR quantity > 500) AND price > 20000;
+
+SELECT * FROM products WHERE name LIKE '%mie%';
+
+SELECT * FROM products WHERE name LIKE '%bakso%';
+
+SELECT * FROM products WHERE name LIKE '%usu%';
+
+SELECT * FROM products WHERE description IS NULL;
+
+SELECT * FROM products WHERE description IS NOT NULL;
+
+SELECT * FROM products WHERE price BETWEEN 10000 AND 20000;
+
+SELECT * FROM products WHERE price NOT BETWEEN 10000 AND 20000;
+
+SELECT * FROM products WHERE category IN ('Makanan', 'Minuman');
+
+SELECT * FROM products WHERE category NOT IN ('Makanan', 'Minuman');
