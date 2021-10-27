775c40c7be7d4b18f8a3689da3f01d70b03003a4
ynw99 <yusufedu99@gmail.com>
2021-10-27 20:05:35 +0700

    index

---
+++ belajar-mysql-dasar/sellers.sql
@@ -0,0 +1,28 @@
+CREATE TABLE sellers (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    name2 VARCHAR(100),
+    name3 VARCHAR(100),
+    email VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (email),
+    INDEX name_index (name),
+    INDEX name2_index (name2),
+    INDEX name3_index (name3),
+    INDEX name1_name2_name3_index (name , name2 , name3)
+)  ENGINE=INNODB;
+
+DROP TABLE sellers;
+
+DESC sellers;
+
+SHOW CREATE TABLE sellers;
+
+SELECT * FROM sellers WHERE name = 'X';
+
+SELECT * FROM sellers WHERE name = 'X' AND name2 = 'X';
+
+SELECT * FROM sellers WHERE name = 'X' AND name2 = 'X' AND name3 = 'X';
+
+ALTER TABLE sellers
+DROP INDEX name_index;
