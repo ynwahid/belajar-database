93375a422c20bc0afd5996584429c21edce9741e
ynw99 <yusufedu99@gmail.com>
2021-10-27 20:39:35 +0700

    full text search

--- belajar-mysql-dasar/products.sql
+++ belajar-mysql-dasar/products.sql
@@ -507,3 +507,37 @@ SET
     price = 500
 WHERE
     id = 'P0016';
+    
+ALTER TABLE products
+ADD FULLTEXT product_fulltext (name, description);
+
+SHOW CREATE TABLE products;
+
+SELECT 
+    *
+FROM
+    products
+WHERE
+    name LIKE '%ayam%'
+        OR description LIKE '%ayam%';
+
+SELECT 
+    *
+FROM
+    products
+WHERE
+    MATCH (name , description) AGAINST ('ayam' IN NATURAL LANGUAGE MODE);
+
+SELECT 
+    *
+FROM
+    products
+WHERE
+    MATCH (name , description) AGAINST ('+ayam -bakso' IN BOOLEAN MODE);
+    
+SELECT 
+    *
+FROM
+    products
+WHERE
+    MATCH (name , description) AGAINST ('bakso' WITH QUERY EXPANSION);
