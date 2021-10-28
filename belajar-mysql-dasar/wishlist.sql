586df07d94a4fd3c6ff77405cb68e7e2b6569e14
ynw99 <yusufedu99@gmail.com>
2021-10-28 11:30:25 +0700

    join

--- belajar-mysql-dasar/wishlist.sql
+++ belajar-mysql-dasar/wishlist.sql
@@ -33,3 +33,54 @@ WHERE
     
 INSERT INTO wishlist(id_product, description)
 VALUES ('Pxxxx', 'Makanan Kesukaan');
+
+SELECT 
+    *
+FROM
+    wishlist
+        JOIN
+    products ON (wishlist.id_product = products.id);
+
+SELECT 
+    w.id AS id_wishlist,
+    p.id AS id_product,
+    p.name AS product_name,
+    w.description AS wishlist_description
+FROM
+    wishlist AS w
+        JOIN
+    products AS p ON (w.id_product = p.id);
+    
+DESCRIBE wishlist;
+
+ALTER TABLE wishlist
+ADD COLUMN id_customer INT;
+
+ALTER TABLE wishlist
+ADD CONSTRAINT fk_wishlist_customer
+FOREIGN KEY (id_customer) REFERENCES customers (id);
+
+SELECT 
+    *
+FROM
+    customers;
+
+UPDATE wishlist 
+SET 
+    id_customer = 1
+WHERE
+    id = 1;
+    
+SELECT 
+    *
+FROM
+    wishlist;
+    
+SELECT 
+    c.email, p.id, p.name, w.description
+FROM
+    wishlist AS w
+        JOIN
+    products AS p ON (w.id_product = p.id)
+        JOIN
+    customers AS c ON (w.id_customer = c.id);
